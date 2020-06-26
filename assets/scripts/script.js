dc4f37e6e6f86ff16f547dc6665f58536d003387
ChinDowns <webdev@chindowns.com>
2020-06-25 23:01:20 -0700

    Fix Play Bugs

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,49 +1,41 @@
 const board = [null, null, null, null, null, null, null, null, null];
-
+const available = [0,1,2,3,4,5,6,7,8]
 $(document).ready(() => {
     const playerMove = () => {
         $(".game-square").on("click", function (event) {
             event.preventDefault();
-            let square = $(this).attr('id')
+            let square = $(this).attr('id');
+            square = +square;
+            console.log(typeof (square));
+            if(available.includes(square)) {
 
             $(this).html("X");
-            // Adds padding for the characters
-            // $(this).addClass("uk-padding-medium-vertical");
-
             board[square] = "X";
-
+            available.splice(available.indexOf(square),1);
+            console.log(`Player chose: ${square}`)
             checkGame("player");
+            }
         });
     }
 
     const computerMove = () => {
         console.log("Computer's Move");
-        let selected = false;
 
-        const tries = board.map((played, index) => {
-            if(played === null ) {
-                return null
-            }else {
-                return index
-            }
-        });
+        let index = Math.floor(Math.random() * available.length);
+        let osquare = available[index];
 
-        console.log(tries);
+        console.log(`Computer Chose: ${osquare}`);
 
-        do {
-            let setSquare = Math.floor(Math.random() * 9);
-            console.log(setSquare);
-            if (tries[setSquare] === null) {
-                selected = true;
-                $(`#${setSquare}`).html("O");
-            }
-        }
-        while (selected === false);
+                $(`#${osquare}`).html("O");
+                board[osquare] = "O";
+                available.splice(available.indexOf(osquare), 1);
+                console.log(`Available Choices: ${available}`);
 
         checkGame("comp");
     }
 
     const checkGame = (turn) => {
+        console.log(`Check for a Win by ${turn}`);
         const row1 = board[0] + board[1] + board[2];
         const row2 = board[3] + board[4] + board[5];
         const row3 = board[6] + board[7] + board[8];
@@ -53,6 +45,7 @@ $(document).ready(() => {
         const diag1 = board[0] + board[4] + board[8];
         const diag2 = board[2] + board[4] + board[6];
 
+        console.log(row1, "\n", row2, "\n", row3, "\n", col1, "\n", col2, "\n", col3, "\n", diag1, "\n",diag2)
 
         if (row1 === "XXX" ||
             row2 === "XXX" ||
@@ -62,9 +55,9 @@ $(document).ready(() => {
             col3 === "XXX" ||
             diag1 === "XXX" ||
             diag2 === "XXX" ) {
-            $("#game").html(`<h1>YOU WIN</h1>`);
+            endGame("You are the Champion");
         }
-        else if (row1 === "OOO" ||
+        if (row1 === "OOO" ||
             row2 === "OOO" ||
             row3 === "OOO" ||
             col1 === "OOO" ||
@@ -72,11 +65,18 @@ $(document).ready(() => {
             col3 === "OOO" ||
             diag1 === "OOO" ||
             diag2 === "OOO") {
-            $("#game").html(`<h1>you lose<h1>`);
+            endGame("Better Luck Next Time");
         }
-        else {
-            nextMove(turn);
-        }
+    
+        nextMove(turn);
+        
+    }
+
+    const endGame = (result) => {
+        const playAgain = $("<a></a>").text("Play Again");
+        $("#results").append(playAgain);
+        const results = $(`<h1>$results</h1>`);
+        $("#results").append(results);
     }
 
     const nextMove = (turn) => {
